de687075b5427deba31598259934bd5f6f72ef81
NottSr <leonardo.br4@hotmail.com>
2022-05-02 21:40:12 -0500

    11. Hello, write

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * check_cycle - function that checks if the list has a cycle
+ * @list: list to check
+ * Return: 0 or 1
+ */
+int check_cycle(listint_t *list)
+{
+	return (0);
+}
