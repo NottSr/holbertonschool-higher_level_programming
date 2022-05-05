1ff5a09900cac9a676ab004a2ae8cb86590f5eb0
NottSr <leonardo.br4@hotmail.com>
2022-05-05 12:21:46 -0500

    3. Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-i, sum = 1, 0
+if __name__ == '__main__':
+    from sys import argv
+    i, sum = 1, 0
 
-while i < len(argv):
-    sum += int(argv[i])
-    i += 1
+    while i < len(argv):
+        sum += int(argv[i])
+        i += 1
 
-print(sum)
+    print(sum)
