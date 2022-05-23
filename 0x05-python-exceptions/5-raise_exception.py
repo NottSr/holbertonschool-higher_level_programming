083ce0c6647a3d415a7135935c64c9b786e7a57d
NottSr <leonardo.br4@hotmail.com>
2022-05-23 13:28:34 -0500

    6. Raise a message

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def raise_exception():
     x = 5
-
     if not type(x) is str:
         raise TypeError()
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    if not type(message) is int:
+        raise NameError(message)
+    
