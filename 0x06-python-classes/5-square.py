f4770eb1b3421722cbb86b4b5e0605dc2d91b71b
NottSr <leonardo.br4@hotmail.com>
2022-05-24 13:06:47 -0500

    0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print('#' * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+**_0x06. Python - Classes and Objects_**
+
+<p><img src="https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/247/oop-meme.jpg" alt="" style=""></p>
