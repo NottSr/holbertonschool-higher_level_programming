fbdf608403c4646c0b768e25ef5e3228f9c79748
NottSr <leonardo.br4@hotmail.com>
2022-05-24 19:11:32 -0500

    3-square fix

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Define a square class"""
+
+
 class Square:
+    """
+    A simple class with the size attribute and error messages
+    """
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -8,6 +14,8 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = size
-
+    """
+    Returns the current square area
+    """
     def area(self):
         return self.__size ** 2
