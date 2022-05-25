45a57c51349a95feed8555c38fa635019551b7b3
NottSr <leonardo.br4@hotmail.com>
2022-05-24 19:05:01 -0500

    1-square fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""define a square class"""
+"""Define a square class"""
 
 
 class Square:
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Define a square class"""
+
+
 class Square:
+    """A simple class with the size attribute"""
     def __init__(self, size):
         self.__size = size
