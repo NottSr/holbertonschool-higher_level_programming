2609123092b3304fe0a5f010a6b2e10bf5c7c4ee
NottSr <leonardo.br4@hotmail.com>
2022-05-30 12:03:42 -0500

    Point 0 and 1 made

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 """
+Module that checks for a NaN value
+"""
+def isNaN(num):
+    """
+    Returns:
+        True if value is NaN
+    """
+    return num!= num
+
+"""
 Module that divides all elements of a matrix.
 """
 def matrix_divided(matrix, div):
@@ -17,25 +27,28 @@ def matrix_divided(matrix, div):
     Returns:
         new_matrix: A new matrix with the div results
     """
-    aux = len(matrix[0])
     new_array = []
     new_matrix = []
+
+    if len(matrix) == 0 or len(matrix[0]) == 0:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    aux = len(matrix[0])
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    if not type(div) in (int, float):
+    if not type(div) in (int, float) or isNaN(div) == True:
         raise TypeError("div must be a number")
 
     for i in range(len(matrix)):
-
-        if not aux is len(matrix[i]):
-            raise TypeError("Each row of the matrix must have the same size")
 
         for j in range(len(matrix[i])):
 
             if not type(matrix[i][j]) in (int, float):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+            if not aux is len(matrix[i]):
+                raise TypeError("Each row of the matrix must have the same size")
 
             num = matrix[i][j]
             new_array.append(round((num / div), 2))
