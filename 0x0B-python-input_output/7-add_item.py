72e7789ebce7fcbc2f6d642c4b47c5c0362a19e9
NottSr <leonardo.br4@hotmail.com>
2022-06-12 16:32:19 -0500

    Task 7 fixed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Script that adds all arguments to a Python list,
+and then save them to a file
+"""
 import os
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
