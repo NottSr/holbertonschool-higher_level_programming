583e8a9e510e7c0fbca90c8cf974a046cb36ee78
NottSr <leonardo.br4@hotmail.com>
2022-07-14 15:14:30 -0500

    task 10 done

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Script that lists all cities contained in the database hbtn_0d_usa.
+-- Each record should display: cities.id - cities.name - states.name
+-- Results must be sorted in ascending order by cities.id
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
