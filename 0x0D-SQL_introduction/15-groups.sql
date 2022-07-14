fb71636ba1bf96c3730aca23c1a05e3708b72b95
NottSr <leonardo.br4@hotmail.com>
2022-07-13 19:22:53 -0500

    Task 15 done

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- Script that lists the number of records with the
+-- same score in the table second_table
+SELECT score, COUNT(score) as number FROM second_table
+GROUP BY score ORDER BY score DESC;
