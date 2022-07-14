e474f3755a1e6fab60ddfea4425a5cda4e1cd043
NottSr <leonardo.br4@hotmail.com>
2022-07-14 13:30:50 -0500

    task 7 done

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
---
+-- Script that creates the database hbtn_0d_usa and the table states.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT NOT NULL AUTO_INCREMENT,
