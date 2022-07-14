85e6d098bfa680d7334f3e2c9e1b6a1912c43fd6
NottSr <leonardo.br4@hotmail.com>
2022-07-14 14:14:58 -0500

    task 9 done

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- Script that lists all cities contained in the database hbtn_0d_usa.
+-- Each record should display: cities.id - cities.name - states.name
+-- Results must be sorted in ascending order by cities.id
+SELECT cities.id, cities.name, states.name FROM states, cities ORDER BY cities.id ASC;
