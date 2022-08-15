dc387e7a073edb010c4c02947a40d9ac43111103
NottSr <leonardo.br4@hotmail.com>
2022-08-15 14:56:20 -0500

    Fixing code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,29 +4,29 @@
 
 const args = process.argv.map(item => item[0]).filter(val => !isNaN(val));
 let i = 2;
-let s_big = 0;
+let sbig = 0;
 let big = 0;
 
 if (args[1]) {
   if (args[0] > args[1]) {
     big = parseInt(args[0]);
-    s_big = parseInt(args[1]);
+    sbig = parseInt(args[1]);
   } else {
     big = parseInt(args[1]);
-    s_big = parseInt(args[0]);
+    sbig = parseInt(args[0]);
   }
 
   while (i < args.length) {
     if (args[i] > big) {
-      s_big = big;
+      sbig = big;
       big = args[i];
-    } else if (args[i] > s_big) {
-      s_big = args[i];
+    } else if (args[i] > sbig) {
+      sbig = args[i];
     }
     i++;
   }
 
-  console.log(s_big);
+  console.log(sbig);
 } else {
   console.log('0');
 }
