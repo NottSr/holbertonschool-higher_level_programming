d8cc30c953f45e0cc5485e80776634eb87f1cc43
NottSr <leonardo.br4@hotmail.com>
2022-08-16 11:09:58 -0500

    Task 11 fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,31 +2,13 @@
 
 /* Script that searches the second biggest integer in the list of arguments. */
 
-const args = process.argv.map(item => item[0]).filter(val => !isNaN(val));
-let i = 2;
-let sbig = 0;
-let big = 0;
+let arr = process.argv.slice(2);
 
-if (args[1]) {
-  if (args[0] > args[1]) {
-    big = parseInt(args[0]);
-    sbig = parseInt(args[1]);
-  } else {
-    big = parseInt(args[1]);
-    sbig = parseInt(args[0]);
-  }
-
-  while (i < args.length) {
-    if (args[i] > big) {
-      sbig = big;
-      big = args[i];
-    } else if (args[i] > sbig) {
-      sbig = args[i];
-    }
-    i++;
-  }
-
-  console.log(sbig);
+if (arr.length < 2) {
+  console.log(0);
 } else {
-  console.log('0');
+  arr = arr.map(Number);
+  arr = [... new Set(arr)];
+  arr = arr.sort((a, b) => {return b - a});
+  console.log(arr[1]);
 }
