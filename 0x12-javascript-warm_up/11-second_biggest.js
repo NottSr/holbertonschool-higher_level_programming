dd55cc30b52c3d65836b1ff362dc44ba290b303c
NottSr <leonardo.br4@hotmail.com>
2022-08-16 11:10:55 -0500

    Task 11 fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,7 +8,7 @@ if (arr.length < 2) {
   console.log(0);
 } else {
   arr = arr.map(Number);
-  arr = [... new Set(arr)];
-  arr = arr.sort((a, b) => {return b - a});
+  arr = [...new Set(arr)];
+  arr = arr.sort((a, b) => { return b - a; });
   console.log(arr[1]);
 }
