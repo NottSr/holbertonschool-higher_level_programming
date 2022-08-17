2bf4553a08774bf86d4eec4dcbfee52a7b9bd826
NottSr <leonardo.br4@hotmail.com>
2022-08-16 20:47:23 -0500

    Task 8 done

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,5 +3,11 @@
 /* Function that returns the reversed version of a list */
 
 exports.esrever = function (list) {
-  return list.sort((a, b) => { return b - a; });
+  const nlist = [];
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    nlist.push(list[i]);
+  }
+
+  return (nlist);
 };
