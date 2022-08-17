244bf25c5b11375542402c9cc2bfe9d66229f460
NottSr <leonardo.br4@hotmail.com>
2022-08-16 20:52:21 -0500

    Task 9 done

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 
-/* Function that returns the reversed version of a list */
+/* Function that prints the number of arguments already printed and the new argument value */
 
+let count = 0;
 
+exports.logMe = function (item) {
+    console.log(count + ": " + item);
+    count += 1;
+}
