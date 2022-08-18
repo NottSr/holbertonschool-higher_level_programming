908aeabf11f589bc06ad56c22e0a668d1b273b32
NottSr <leonardo.br4@hotmail.com>
2022-08-18 11:24:49 -0500

    Task 100 done

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/* Script that prints the number of movies where the character “Wedge Antilles” is present */
+
+const arg = process.argv[2];
+const axios = require('axios').default;
+
+axios.get('https://swapi-api.hbtn.io/api/films/' + arg)
+  .then(function (response) {
+    // Success execution
+    for (const key of response.data.characters) {
+      axios.get(key)
+      .then((res) => {
+        console.log(res.data.name);
+      });
+    }
+  });
