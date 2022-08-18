0e8f01e8b3b187bce0b2dd15745b524c05be7e75
NottSr <leonardo.br4@hotmail.com>
2022-08-18 11:30:45 -0500

    Task 101 done and fixing some style

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,10 +2,9 @@
 
 /* Script that prints all characters of a Star Wars movie */
 
-const arg = process.argv[2];
 const axios = require('axios').default;
 
-axios.get('https://swapi-api.hbtn.io/api/films/' + arg)
+axios.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2])
   .then(function (response) {
     // Success execution
     for (const key of response.data.characters) {
