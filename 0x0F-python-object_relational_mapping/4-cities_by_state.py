83cd1d00ea8b872bd4cd480b031e31bf61603e97
NottSr <leonardo.br4@hotmail.com>
2022-08-30 11:00:23 -0500

    Task 4 done

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Script that lists all cities from the database hbtn_0e_0_usa
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    Condition that makes the file executable only if it's executed directly
+    """
+    def DBConection():
+        """
+        Function that connects a database and print "cities" info
+        """
+        try:
+            DB_Conect = MySQLdb.connect(
+                "localhost",
+                argv[1],
+                argv[2],
+                argv[3]
+            )
+        except Exception:
+            print("Couldn't connect to database")
+            return (0)
+
+        cursor = DB_Conect.cursor()
+        cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+                LEFT JOIN states \
+                ON cities.state_id = states.id \
+                ORDER BY cities.id ASC")
+
+        list = cursor.fetchall()
+
+        for item in list:
+            print(item)
+
+        cursor.close()
+        DB_Conect.close()
+
+    DBConection()
