abd6f2e4f3f314a75d9c89cea0df94ba5c5318c5
NottSr <leonardo.br4@hotmail.com>
2022-09-02 10:10:27 -0500

    Update task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     Script that fetches https://intranet.hbtn.io/status
     """
     html = requests.get("https://intranet.hbtn.io/status")
-    print(f"Body response:\n\
-    - type: {type(html.text)}\n\
-    - content: {html.text}")
+    print("Body response: \n\t- type: {}\n\t- content: {}".format(
+        type(html.text),
+        html.text
+        ))
